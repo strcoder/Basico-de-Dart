386e77cd262f129ef09de22d503b42904bf57587
Antonio Guzman <aguzmanv09@gmail.com>
2020-01-23 19:39:05 -0600

    Uso basico de listas y sets

--- Basico-1/web/main.dart
+++ Basico-1/web/main.dart
@@ -1,8 +1,10 @@
 import 'dart:html';
 
 void main(List<String> arguments) {
-  variables();
-  variablesString();
+  // variables();
+  // variablesString();
+  listas();
+  coleccionesSet();
 }
 
 void variables () {
@@ -49,4 +51,56 @@ void variablesString() {
 
   // Metodos de los Strings
   print(varString.isEmpty);
+}
+
+void listas() {
+  // List lista; // Si no se inicializa sera igual a NULL // Se inicializa con valores no definidos
+  List<String> lista; // Si no se inicializa sera igual a NULL // Se inicializa con valores definidos tipo String
+
+  List<String> listaAux;
+
+  // lista = []; // Se inicializa pero esta vacia
+
+  // lista = ['Azul', 'Rojo', 'Negro']; 
+
+  lista = ['Azul', 'Rojo', 'Negro']; // Se inicializa con valores no definidos
+
+  print(lista);
+
+  lista.add('Rosa'); // Agregar elementos dentro de la lista
+
+  print(lista);
+
+  lista.removeLast(); // Eliminar el ultimo elemento
+
+  print(lista);
+
+  listaAux = ['Verde', 'Blanco'];
+
+  lista.addAll(listaAux);
+
+  print(lista);
+
+  lista.removeAt(2);
+
+  print(lista);
+}
+
+void coleccionesSet() {
+  // Set set; // Si no se inicializa sera igual a NULL // Se inicializa con valores no definidos
+  Set set; // Si no se inicializa sera igual a NULL // Se inicializa con valores no definidos
+
+  set = Set.from(['Daniela', 'Antonio', 'Karen', 'Pedro', 'Juan']); // Inicializar set
+
+  print(set);
+
+  set.add('Anna'); // Agregar elementos
+
+  print(set);
+
+  set.remove('Antonio'); // Eliminar elementos dentro del set - Se debe pasar el objeto que se desea eliminar
+
+  print(set);
+
+
 }
