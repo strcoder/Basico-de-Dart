c5ceb49a00644ec5aade8ab0b36ccb86b8d33e92
Antonio Guzman <aguzmanv09@gmail.com>
2020-01-25 09:39:15 -0600

    Declaracion de funciones basicas

--- Basico-2/web/main.dart
+++ Basico-2/web/main.dart
@@ -1,5 +1,24 @@
 import 'dart:html';
 
+// Funcion de alto nivel que debe tener cualquier programa
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  add();
+  addParam(3, 5);
+  suma(5, 3);
+}
+
+// Declaracion de funciones
+// Funcion basica
+void add() {
+  print(3 + 5);
+}
+
+// Funcion con paramentros
+void addParam(int a, int b) {
+  print(a + b);
+}
+
+//Funcion con retorno de valor y parametros
+int suma(int a, int b) {
+  return a + b;
 }
