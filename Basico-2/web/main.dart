197858ddf07a3a5b9f10ee16442bbf063f574b10
Antonio Guzman <aguzmanv09@gmail.com>
2020-01-25 09:51:46 -0600

    Uso de arrow functions y lambdas

--- Basico-2/web/main.dart
+++ Basico-2/web/main.dart
@@ -1,13 +1,23 @@
 import 'dart:html';
 
+// Declaracion de funciones
+
 // Funcion de alto nivel que debe tener cualquier programa
 void main() {
   add();
   addParam(3, 5);
-  suma(5, 3);
+  print(suma(5, 3));
+  arrowSuma(3, 5);
+  print(arrowRetrunSuma(3, 5));
+
+  // Funciones Lambda 
+  List<String> list = ['Azul', 'Rojo', 'Blaco'];
+  
+  list.forEach((value){
+    print(value);
+  });
 }
 
-// Declaracion de funciones
 // Funcion basica
 void add() {
   print(3 + 5);
@@ -22,3 +32,9 @@ void addParam(int a, int b) {
 int suma(int a, int b) {
   return a + b;
 }
+
+//Arrow Functions sin retorno de valor
+void arrowSuma(int a, int b) => print('El resultado es: ${a + b}');
+
+//Arrow Functions con retorno de valor
+int arrowRetrunSuma(int a, int b) => a + b;
