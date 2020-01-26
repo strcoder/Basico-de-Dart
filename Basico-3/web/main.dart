5d8c1889214b285d4f4a594e235403d78c7f617e
Antonio Guzman <aguzmanv09@gmail.com>
2020-01-25 20:24:04 -0600

    Expresiones condicionlaes

--- Basico-3/web/main.dart
+++ Basico-3/web/main.dart
@@ -1,5 +1,25 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  expresionesCondicionales();
+}
+
+void expresionesCondicionales() {
+  var edad = 29;
+
+  if (edad >= 18){
+    print('adulto');
+  } else if (edad < 18){
+    print('joven');
+  } else if (edad > 50 && edad < 90){
+    print('tercera edad');
+  } else {
+    print('error de edad');
+  }
+   
+  var acceso = edad >= 18 ? 'correcto':'incorrecto';
+  print ('El usuario tine acceso $acceso');
+   
+  var nombre;
+  print(nombre ?? 'invitado');
 }
