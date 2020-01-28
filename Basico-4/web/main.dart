c7fd3b866cde71b2e1f26b2885d9d3a8a182579a
Antonio Guzman <aguzmanv09@gmail.com>
2020-01-28 17:27:10 -0600

    Implementacion de clases en dart

--- Basico-4/web/main.dart
+++ Basico-4/web/main.dart
@@ -1,5 +1,35 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  // Creacion de objeto de tipo empleado
+  var empleado = Empleado();
+  empleado.id = 123;
+  empleado.name = 'Karen';
+
+  if (empleado.cumplioHorario()) {
+    empleado.trabajar();
+  }
+
+  // Otro modo de inicializar objetos
+  var empleado2 = Empleado()
+  ..id = 123
+  ..name = 'Antonio';
+
+  if (empleado2.cumplioHorario()) {
+    empleado2.trabajar();
+  }
+}
+
+// Calse empleado
+class Empleado {
+  var id;
+  var name;
+
+  bool cumplioHorario() {
+    return true;
+  }
+
+  void trabajar() {
+    print('El empleado $name realizo su trabajo');
+  }
 }
